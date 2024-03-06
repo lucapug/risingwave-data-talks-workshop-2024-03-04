06d4767001f201d7079c2ff50dbe0c80830d3118
lucapug <lucapug@gmail.com>
2024-03-06 23:37:01 +0100

    avg_fare_amount_per_min not per hour

--- clickhouse-sql/avg_fare_amt.sql
+++ clickhouse-sql/avg_fare_amt.sql
@@ -1,5 +1,5 @@
-CREATE TABLE avg_fare_amt(
-    avg_fare_amount_per_hour numeric,
-    num_rides_per_hour Int64,
+CREATE TABLE avg_fare_amt_1_min(
+    avg_fare_amount_per_min numeric,
+    num_rides_per_min Int64,
 ) ENGINE = ReplacingMergeTree
-PRIMARY KEY (avg_fare_amount_per_hour, num_rides_per_hour);
+PRIMARY KEY (avg_fare_amount_per_min, num_rides_per_min);
